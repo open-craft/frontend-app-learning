896b71c956c870069e8432874925dee33836a907
Pooja Kulkarni <pooja@opencraft.com>
2026-06-29 15:01:26 -0400

    feat: add missing plugin slots for syllabus tab

--- src/course-tabs/CourseTabsNavigation.tsx
+++ src/course-tabs/CourseTabsNavigation.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import classNames from 'classnames';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import { CoursewareSearch, CoursewareSearchToggle } from '../course-home/courseware-search';
 import { useCoursewareSearchState } from '../course-home/courseware-search/hooks';
 
+import { CourseTabLinksSlot } from '../plugin-slots/CourseTabLinksSlot';
 import Tabs from '../generic/tabs/Tabs';
 import messages from './messages';
 
@@ -32,15 +32,7 @@ const CourseTabsNavigation = ({
               className="nav-underline-tabs"
               aria-label={intl.formatMessage(messages.courseMaterial)}
             >
-              {tabs.map(({ url, title, slug }) => (
-                <a
-                  key={slug}
-                  className={classNames('nav-item flex-shrink-0 nav-link', { active: slug === activeTabSlug })}
-                  href={url}
-                >
-                  {title}
-                </a>
-              ))}
+              <CourseTabLinksSlot tabs={tabs} activeTabSlug={activeTabSlug} />
             </Tabs>
           </div>
           <div className="search-toggle">
